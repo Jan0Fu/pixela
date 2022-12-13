702b968d781f144be9c45ae3f06ed27ebd931211
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-13 12:53:56 +0100

    Finished whole application

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
-
+from datetime import datetime
 USERNAME = "jan0fu"
 TOKEN = "9jf0sdf3jslf43jds34jdsf4s78"
+GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
     "token": TOKEN,
@@ -14,7 +15,7 @@ user_params = {
 # print(response.text)
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "Hours",
     "type": "float",
@@ -25,5 +26,18 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+today = datetime.now()
+post_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "1.5"
+}
+post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+#response = requests.post(url=post_endpoint, json=post_config, headers=headers)
+
+put_date = "20221212"
+put_config = {"quantity": "1.0"}
+put_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{put_date}"
+response = requests.put(url=put_endpoint, json=put_config, headers=headers)
+print((response.text))
